cf6caa00b6a35626113cfed8745fc851e4378d27
Lakshay Mittal <lakshayjain0611@gmail.com>
2025-01-26 17:00:53 +0530

    skill page

--- src/skill.js
+++ src/skill.js
@@ -7,36 +7,36 @@ export default function Skills(){
         
          <div id='firstblock'>
             <div id='inner' >
-                <h1 id='skill_name'>Machine Learning <a href='https://portfolio-blond-nine-41.vercel.app/'  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
+                <h1 id='skill_name'>Machine Learning <a href='\LAKSHAY MITTAL (12).pdf' target="_blank" rel="noopener noreferrer"  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
                 <div id='percentage'><div id='filled'></div><div id='empty'></div></div>
             </div>
 
             <div >
-                <h1 id='skill_name'> Deep Learning <a href='https://portfolio-blond-nine-41.vercel.app/'  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
+                <h1 id='skill_name'> Deep Learning <a href='\LAKSHAY MITTAL (12).pdf' target="_blank" rel="noopener noreferrer"  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
                 <div id='percentage'><div id='filled'></div><div id='empty'></div></div>
             </div>
 
 
             <div >
-                <h1 id='skill_name'>Standard Query Lang. <a href='https://portfolio-blond-nine-41.vercel.app/'  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
+                <h1 id='skill_name'>Standard Query Lang. <a href='\LAKSHAY MITTAL (12).pdf' target="_blank" rel="noopener noreferrer"  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
                 <div id='percentage'><div id='filled'></div><div id='empty'></div></div>
             </div>
             
          </div>
          <div id='firstblock'>
             <div >
-                <h1 id='skill_name'>ReactJS <a href='https://portfolio-blond-nine-41.vercel.app/'  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
+                <h1 id='skill_name'>ReactJS <a href='\LAKSHAY MITTAL (12).pdf' target="_blank" rel="noopener noreferrer"  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
                 <div id='percentage'><div id='filled'></div><div id='empty'></div></div>
             </div>
 
             <div >
-                <h1 id='skill_name'>HTML / CSS<a href='https://portfolio-blond-nine-41.vercel.app/'  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
+                <h1 id='skill_name'>HTML / CSS<a href='\LAKSHAY MITTAL (12).pdf' target="_blank" rel="noopener noreferrer"  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
                 <div id='percentage'><div id='filled'></div><div id='empty'></div></div>
             </div>
 
 
             <div >
-                <h1 id='skill_name'>Data Structure Alg<a href='https://portfolio-blond-nine-41.vercel.app/'  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
+                <h1 id='skill_name'>Data Structure Alg<a href='\LAKSHAY MITTAL (12).pdf' target="_blank" rel="noopener noreferrer"  style={{fontSize:"40px" , color:"black"}}>+</a></h1>
                 <div id='percentage'><div id='filled'></div><div id='empty'></div></div>
             </div>
             
